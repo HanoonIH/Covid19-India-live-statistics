74f3ce6a5a4713de329daaf49b88ada624695a45
HanoonIH <hanoonih123@gmail.com>
2020-07-22 16:04:30 +0530

    chart type to line

--- public/script.js
+++ public/script.js
@@ -7,6 +7,25 @@ $(document).ready(function(){
 
         var totalConfirmed, totalRecovered, totalActive, totalDeath;
 
+        var state = [];
+        var confirmed = [];
+        var recovered = [];
+        var deaths = [];
+
+        $.each(data.statewise, function(id, obj){
+            state.push(obj.state);
+            confirmed.push(obj.confirmed);
+            recovered.push(obj.recovered);
+            deaths.push(obj.deaths);
+        })
+
+        state.shift()
+        confirmed.shift()
+        recovered.shift()
+        deaths.shift()
+
+        console.log(state)
+
         totalConfirmed = data.statewise[0].confirmed;
         totalRecovered = data.statewise[0].recovered;
         totalActive = data.statewise[0].active;
@@ -17,26 +36,37 @@ $(document).ready(function(){
         $('#active').append(totalActive);
         $('#death').append(totalDeath);
 
+        var myChart = document.getElementById('myChart').getContext('2d');
+
+        var chart = new Chart(myChart, {
+            type: 'line',
+            data: {
+                labels: state,
+                datasets: [
+                    {
+                        label: "Confirmed",
+                        data: confirmed,
+                        backgroundColor: "#ffcd56",
+                        minBarLength: 100
+                    },
+                    {
+                        label: "Recovered",
+                        data: recovered,
+                        backgroundColor: "#36a2eb",
+                        minBarLength: 100
+                    },
+                    {
+                        label: "Deaths",
+                        data: deaths,
+                        backgroundColor: "#ff6384",
+                        minBarLength: 100
+                    }
+                ]
+            },
+            options: {}
+
+        })
+
     })
-
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'doughnut',
-
-        // The data for our dataset
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [{
-                label: 'My First dataset',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [0, 10, 5, 2, 20, 30, 45]
-            }]
-        },
-
-        // Configuration options go here
-        options: {}
-    });
 })
 
